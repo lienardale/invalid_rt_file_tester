bdd105d8f238f6b8aee29699478b0958c99d66f8
Alienard <alienard@student.42.fr>
2020-03-04 14:50:06 +0100

    non existing file and .cube file added, close of the windows in case of errors implemented, need to implement a diff and a comprehensible output

--- invalid_rt.sh
+++ invalid_rt.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
-rm -f test.log
+rm -f kill.log
+function run(){ rm -f test.log
 cd ../
 make
 cd invalid_rt_file_tester
-i=0
+echo '' >> test.log
+echo "Test non-exist:" >> test.log
+./../miniRT srcs/error.rt >> test.log &
+ps -ef | grep "miniRT" | grep -v 'grep' | awk '{print $2}' | xargs kill 1>&2
+echo '' >> test.log
+echo "Test .cube:" >> test.log
+./../miniRT srcs/0.cube >> test.log &
+ps -ef | grep "miniRT" | grep -v 'grep' | awk '{print $2}' | xargs kill 1>&2
+i=0 
 while true; do
 	if [[ "$i" -gt 190 ]]; then
 		exit 1
 	fi
-	./../miniRT srcs/$i.rt >>test.log
-	echo Test $i
+	echo '' >> test.log
+	echo "Test $i:" >> test.log
+	./../miniRT srcs/$i.rt >> test.log &
+	sleep 0.1
+	ps -ef | grep "miniRT" | grep -v 'grep' | awk '{print $2}' | xargs kill 1>&2
+	# echo Test $i
 	((i++))
-done
+done }
+run 2>> kill.log
+
+DIFF=$(diff kill.log srcs/kill_diff.log)
+echo "$DIFF"
+echo | diff test.log srcs/test_diff.log
